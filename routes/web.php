ba68b5b2c752166848aeaefc31c31e0e7c259a52
devYuMinKim <gimyumin40@gmail.com>
2023-04-12 06:32:48 +0900

    chore : install Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Attendance\GetAttendance; // 출석 페이지 컨트롤러
 
-// DB 참석 테이블에 유저의 id로 값을 찾아보고 없으면 출석 버튼 동작하도록 하는 데이터 반환
-// 있으면 이미 출석했다는 데이터 반환
-Route::get('/', [GetAttendance::class, 'isAttend']); // GetAttendance 컨트롤러 isAttend메서드 호출
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-// 출석 가능한지 확인
-// 유저의 위치정보와 DB의 강의실 위치의 대조 미들웨어 만들어야 함
-Route::post('/', function () {
-    return response()->json($data = 'ok', 200);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// 출석 요청
-// DB에 접근해 해당 유저id 참석 테이블에 추가하는 컨트롤러 필요함
-Route::put('/', function () {
-    return response()->json($data = '출석 완료', 200);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
