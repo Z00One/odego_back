ab647f77f5a2aaefe0ec3cac2ca273e7a4f81477
Z00One <9000248@g.yju.ac.kr>
2023-05-01 14:56:52 +0900

    Add missing router in web.php after pulling changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Attendance\GetAttendance; // 출석 페이지 컨트롤러
 
 /*
 |--------------------------------------------------------------------------
@@ -40,3 +41,19 @@ Route::get('/login/google', [AuthenticatedSessionController::class, 'create'])
 Route::get('/register', [RegisteredUserController::class, 'create'])
         ->middleware('guest')
         ->name('register');
+
+// DB 참석 테이블에 유저의 id로 값을 찾아보고 없으면 출석 버튼 동작하도록 하는 데이터 반환
+// 있으면 이미 출석했다는 데이터 반환
+Route::get('/', [GetAttendance::class, 'isAttend']); // GetAttendance 컨트롤러 isAttend메서드 호출
+
+// 출석 가능한지 확인
+// 유저의 위치정보와 DB의 강의실 위치의 대조 미들웨어 만들어야 함
+Route::post('/', function () {
+    return response()->json($data = 'ok', 200);
+});
+
+// 출석 요청
+// DB에 접근해 해당 유저id 참석 테이블에 추가하는 컨트롤러 필요함
+Route::put('/', function () {
+    return response()->json($data = '출석 완료', 200);
+});
